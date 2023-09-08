22a1deee8e606a8c161fda77a69b9a03f5651d1c
Harshit Mishra <harshitmishra1818@gmail.com>
2023-09-08 19:07:22 +0530

    Second changes in requierments.txt and app.py

--- Loan_Pridiction_WebApp/app.py
+++ Loan_Pridiction_WebApp/app.py
@@ -4,7 +4,7 @@ import pickle
 import numpy as np
 
 app = Flask(__name__)
-model = pickle.load(open('model.pkl', 'rb'))
+model = pickle.load(open('Loan_Pridiction_WebApp\model.pkl', 'rb'))
 
 @app.route('/')
 def home():
